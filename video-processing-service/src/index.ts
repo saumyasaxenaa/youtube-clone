cca78c1dcb67f346c07fb77ae4d70e6afdf2bf93
saumyasaxenaa <saumyasaxena004@gmail.com>
2023-07-26 18:18:20 -0700

    added uploading videos to firestore and UI functionality

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -7,6 +7,7 @@ import {
   setupDirectories,
   uploadProcessedVideo,
 } from "./storage";
+import { isVideoNew, setVideo } from "./firestore";
 
 setupDirectories();
 
@@ -29,11 +30,28 @@ app.post("/process-video", async (req, res) => {
     return res.status(400).send("Bad Request: missing filename.");
   }
 
-  const inputFileName = data.name;
+  const inputFileName = data.name; // Format of <UID>-<DATE>.<Extension>
   const outputFileName = `processed-${inputFileName}`;
+  const videoId = inputFileName.split(".")[0];
 
+  if (!isVideoNew(videoId)) {
+    return res
+      .status(400)
+      .send("Bad Request: video already processing or processed");
+  } else {
+    await setVideo(videoId, {
+      id: videoId,
+      uid: videoId.split("-")[0],
+      status: "processing",
+    });
+  }
   // Download the raw video from cloud storage
   await downloadRawVideo(inputFileName);
+
+  await setVideo(videoId, {
+    status: "processed",
+    filename: outputFileName,
+  });
 
   // Convert the video to 360p
   try {
